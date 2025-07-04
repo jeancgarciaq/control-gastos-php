84e52c727dc8026ae3cf92f471f890332f1116d2
jeancgarciaq <jcvictory@hotmail.com>
2025-07-04 17:04:11 -0400

    add: El request de Register

---
+++ app/Requests/RegisterRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Requests;
+
+use App\Core\Validator;
+
+/**
+ * Class RegisterRequest
+ * Handles validation for registration data.
+ */
+class RegisterRequest
+{
+    private Validator $validator;
+    private array $errors = [];
+
+    public function __construct()
+    {
+        $this->validator = new Validator();
+    }
+
+    /**
+     * Validates the given registration data.
+     *
+     * @param array $data The registration data to validate.
+     * @return bool True if the data is valid, false otherwise.
+     */
+    public function validate(array $data): bool
+    {
+        $rules = [
+            'username' => 'required|min:3',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'g-recaptcha-response' => 'required', // Add this line
+        ];
+
+        if (!$this->validator->validate($data, $rules)) {
+            $this->errors = $this->validator->errors();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Gets the validation errors.
+     *
+     * @return array An associative array of validation errors.
+     */
+    public function errors(): array
+    {
+        return $this->errors;
+    }
+}
