4c7df61cd3164f88354ecf62ce4bc71cda491b40
jeancgarciaq <jcvictory@hotmail.com>
2025-07-04 17:51:16 -0400

    feat: Modifique tabla de user a users

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS expense_control CHARACTER SET utf8mb4 COLLATE utf8
 USE expense_control;
 
 -- Create the user table
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS profile (
     children INT DEFAULT 0,
     assets DECIMAL(15, 2) DEFAULT 0.00,
     initial_balance DECIMAL(15, 2) DEFAULT 0.00,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Create the expenses table
